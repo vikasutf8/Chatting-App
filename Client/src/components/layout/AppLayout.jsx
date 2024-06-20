e2354cb16d679acce64802871d923c831ac580ef
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-06-20 15:22:52 +0530

    updated header UI

--- Client/src/components/layout/AppLayout.jsx
+++ Client/src/components/layout/AppLayout.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Header from './Header'
 import Title from '../shared/Title'
-import { Grid ,Container} from '@mui/material'
+import { Grid ,} from '@mui/material'
 
 const AppLayout = () => (WrappedComponent) => {
     return (props) => {
@@ -10,15 +10,20 @@ const AppLayout = () => (WrappedComponent) => {
             <>
                 <Title />
                 <Header />
-
-                <Grid Container height={"calc(100vh -4rem)"}>
-                    <Grid item xs={4} height={"100%"} >
+                <Grid container height={"calc(100vh - 4rem)"}>
+                    <Grid item sm={4} md={3}  sx={{
+                        display: { xs: 'none', sm: 'block' },
+                    }} height={"100%"} >
                         first
                     </Grid>
-                    <Grid item xs={4} height={"100%"} bgcolor="primary.main">
+                    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"} >
                         <WrappedComponent {...props} />
                     </Grid>
-                    <Grid item xs={4} height={"100%"}>
+                    <Grid item md={4} lg={3}sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        padding: "2rem",
+                        bgcolor: "rgba(0,0,0,0.85)"
+                    }} height={"100%"} >
                         first
                     </Grid>
                 </Grid>
--- Client/src/components/layout/Header.jsx
+++ Client/src/components/layout/Header.jsx
@@ -1,8 +1,61 @@
+import { AppBar, Box, Icon, IconButton, Toolbar, Tooltip, Typography } from '@mui/material'
 import React from 'react'
-
+import { orange } from '../../constants/color'
+import {useNavigate} from 'react-router-dom'
+import { Menu as MenuIcon, Search as SearchIcon, Add as AddIcon, Group as GroupIcon} from '@mui/icons-material/'
 const Header = () => {
+  const navigate =useNavigate();
+  const handleMobile = () => {
+
+  }
+  const operSearchDialog = () => { }
+  const openNewGroup = () => { }
+  const navigateToGroup = () => navigate("/groups")
   return (
-    <div>Header</div>
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar position="static" sx={{
+          bgcolor: orange,
+        }}>
+          <Toolbar>
+
+
+            <Typography variant="h6"
+              sx={{
+                display: { xs: 'none', sm: 'block' },
+              }}>
+              Chatt App
+            </Typography>
+            <Box sx={{
+              display: { xs: 'block', sm: 'none' },
+
+            }}>
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box >
+              <Tooltip title="Search Icon">
+                <IconButton color="inherit" size="large" onClick={operSearchDialog}>
+                  <SearchIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="New Group">
+                <IconButton color="inherit" size="large" onClick={openNewGroup}>
+                  <AddIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Manage Groups">
+              <IconButton color="inherit" size="large" onClick={navigateToGroup}>
+                 <GroupIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   )
 }
 
