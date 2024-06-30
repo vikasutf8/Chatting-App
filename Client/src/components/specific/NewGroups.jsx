28c16a079ef562e3c4a3b6629694ab15b54c6a6f
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-06-30 23:53:08 +0530

    error hadling

--- Client/src/components/specific/NewGroups.jsx
+++ Client/src/components/specific/NewGroups.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import { Avatar, Button, Dialog, DialogTitle, InputAdornment, List, ListItem, Stack, TextField, Typography } from '@mui/material'
 const NewGroups = () => {
   return (
-    <div>
-      
-    </div>
+    <Dialog open>
+    <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"20rem"}>
+      <DialogTitle>Notifications</DialogTitle>
+    </Stack>
+    {
+      sampleNotifications.length > 0 ? (
+        sampleNotifications.map((i) => <NotificationItem
+          sender={i.sender}
+          _id={i._id}
+          handler={friendRequestHandler}
+          key={i._id}
+        />)
+      ) : (
+        <Typography textAlign={"center"}> 0 notifications</Typography>
+      )
+    }
+  </Dialog>
   )
 }
 
--- Client/src/components/specific/Notifications.jsx
+++ Client/src/components/specific/Notifications.jsx
@@ -4,67 +4,69 @@ import { sampleNotifications } from '../../constants/sampleData'
 
 const Notifications = () => {
 
-  const friendRequestHandler = ({_id,accept}) => {
+  const friendRequestHandler = ({ _id, accept }) => {
     console.log(_id)
   }
 
-  return <Dialog open>
-    <Stack p={{xs:"1rem",sm:"2rem"}} maxWidth={"20rem"}>
-        <DialogTitle>Notifications</DialogTitle>
+  return (
+    <Dialog open>
+    <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"20rem"}>
+      <DialogTitle>Notifications</DialogTitle>
     </Stack>
     {
-      sampleNotifications.length >0?(
-        sampleNotifications.map((i) =><NotificationItem 
-          sender={i.sender} 
+      sampleNotifications.length > 0 ? (
+        sampleNotifications.map((i) => <NotificationItem
+          sender={i.sender}
           _id={i._id}
           handler={friendRequestHandler}
           key={i._id}
         />)
-      ): (
+      ) : (
         <Typography textAlign={"center"}> 0 notifications</Typography>
       )
     }
   </Dialog>
+  )
 }
 
-const NotificationItem = memo(({sender,_id,handler}) => {
-  const {name,avatar} = sender
+const NotificationItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender
   return (
     <ListItem>
-        <Stack
-            direction={"row"}
-            alignItems={"center"}
-            spacing={"1rem"}
-            width={"100%"}
-            // {...styling}
-        >
-            <Avatar src={"avatar"}/>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      // {...styling}
+      >
+        <Avatar src={"avatar"} />
 
-            <Typography
-               variant="body1"
-               sx={{
-                 flexGlow: 1,
-                 display: "-webkit-box",
-                 WebkitLineClamp: 1,
-                 WebkitBoxOrient: "vertical",
-                 overflow: "hidden",
-                 textOverflow: "ellipsis",
-                 width: "100%",
-               }}>
-                {`${name} sent you friend request. `}</Typography>
-            <Stack direction={{
-              xs:"column",
-              
-            }}
-              >
-              <Button color="success"onClick={()=> handler({_id,accept:true})}>
-                Accept
-              </Button>
-              <Button color="error" onClick={()=> handler({_id,accept:false})}>
-                Reject
-              </Button>
-            </Stack>
+        <Typography
+          variant="body1"
+          sx={{
+            flexGlow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%",
+          }}>
+          {`${name} sent you friend request. `}</Typography>
+        <Stack direction={{
+          xs: "column",
+
+        }}
+        >
+          <Button color="success" onClick={() => handler({ _id, accept: true })}>
+            Accept
+          </Button>
+          <Button color="error" onClick={() => handler({ _id, accept: false })}>
+            Reject
+          </Button>
         </Stack>
+      </Stack>
     </ListItem>
   )
 })
