0ded11224ff76087b40c39759bab91dc04f7da88
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-06-30 23:51:19 +0530

    NOtification UI done

--- Client/src/components/specific/Notifications.jsx
+++ Client/src/components/specific/Notifications.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { Avatar, Button, Dialog, DialogTitle, InputAdornment, List, ListItem, Stack, TextField, Typography } from '@mui/material'
+import { sampleNotifications } from '../../constants/sampleData'
 
 const Notifications = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  const friendRequestHandler = ({_id,accept}) => {
+    console.log(_id)
+  }
+
+  return <Dialog open>
+    <Stack p={{xs:"1rem",sm:"2rem"}} maxWidth={"20rem"}>
+        <DialogTitle>Notifications</DialogTitle>
+    </Stack>
+    {
+      sampleNotifications.length >0?(
+        sampleNotifications.map((i) =><NotificationItem 
+          sender={i.sender} 
+          _id={i._id}
+          handler={friendRequestHandler}
+          key={i._id}
+        />)
+      ): (
+        <Typography textAlign={"center"}> 0 notifications</Typography>
+      )
+    }
+  </Dialog>
 }
+
+const NotificationItem = memo(({sender,_id,handler}) => {
+  const {name,avatar} = sender
+  return (
+    <ListItem>
+        <Stack
+            direction={"row"}
+            alignItems={"center"}
+            spacing={"1rem"}
+            width={"100%"}
+            // {...styling}
+        >
+            <Avatar src={"avatar"}/>
+
+            <Typography
+               variant="body1"
+               sx={{
+                 flexGlow: 1,
+                 display: "-webkit-box",
+                 WebkitLineClamp: 1,
+                 WebkitBoxOrient: "vertical",
+                 overflow: "hidden",
+                 textOverflow: "ellipsis",
+                 width: "100%",
+               }}>
+                {`${name} sent you friend request. `}</Typography>
+            <Stack direction={{
+              xs:"column",
+              
+            }}
+              >
+              <Button color="success"onClick={()=> handler({_id,accept:true})}>
+                Accept
+              </Button>
+              <Button color="error" onClick={()=> handler({_id,accept:false})}>
+                Reject
+              </Button>
+            </Stack>
+        </Stack>
+    </ListItem>
+  )
+})
 
 export default Notifications
--- Client/src/constants/sampleData.js
+++ Client/src/constants/sampleData.js
@@ -42,3 +42,53 @@ export const samepleChats = [
       _id: "2",
     },
   ];
+
+
+  export const sampleNotifications = [
+    {
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "John Doe",
+      },
+      _id: "1",
+    },
+    {
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "John Boi",
+      },
+      _id: "2",
+    },
+  ];
+  
+  export const sampleMessage = [
+    {
+      attachments: [],
+      content: "L*uda ka Message hai",
+      _id: "sfnsdjkfsdnfkjsbnd",
+      sender: {
+        _id: "user._id",
+        name: "Chaman ",
+      },
+      chat: "chatId",
+      createdAt: "2024-02-12T10:41:30.630Z",
+    },
+  
+    {
+      attachments: [
+        {
+          public_id: "asdsad 2",
+          url: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      ],
+      content: "",
+      _id: "sfnsdjkfsdnfkdddjsbnd",
+      sender: {
+        _id: "sdfsdfsdf",
+        name: "Chaman  2",
+      },
+      chat: "chatId",
+      createdAt: "2024-02-12T10:41:30.630Z",
+    },
+  ];
+  
