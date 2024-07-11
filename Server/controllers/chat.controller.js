cfad9054612e570a81c595cd7cd682fef7522ccc
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-07-11 14:29:35 +0530

    rename group api created

--- Server/controllers/chat.controller.js
+++ Server/controllers/chat.controller.js
@@ -314,6 +314,26 @@ const getChatDetail = TryCatch(async (req, res, next) => {
   }
 });
 
+const renameGroup = TryCatch(async (req, res, next) => {
+ const chatId = req.params._id;
+ const { name } = req.body;
+
+  const chat = await Chat.findById(chatId);
+  if(!chat) return next(new ErrorHandler("Chat not found", 404));
+  if(!chat.groupChat) return next(new ErrorHandler("This is not a group chat", 400));
+  if(chat.creator.toString() !== req.user.toString()) return next(new ErrorHandler("You are not authorized to rename the group", 403));
+
+  chat.name=  name
+  await chat.save();
+
+  emitEvent(req,REFETCH_CHATS,chat.members);
+  return res.status(200).json({
+    success:true,
+    message:"Group renamed successfully",
+    chat
+  });
+});
+
 export {
   newGroupChat,
   getMyChats,
@@ -323,4 +343,5 @@ export {
   leaveGroup,
   sendAttachments,
   getChatDetail,
+  renameGroup,
 };
