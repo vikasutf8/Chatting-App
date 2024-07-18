54b36c8f3c70eaf03a35d63d73c1ece7ff2b90c0
vikasutf8 <21bcs241@iiitdmj.ac.in>
2024-07-18 19:27:30 +0530

    implementing Socket.io and listening at server

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,20 @@
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import express from 'express';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import { Server } from "socket.io";
+import {createServer} from "http"
 
-
-
-import { errorMiddleware } from './middlewares/error.js';
-import chatRoutes from './routes/chat.routes.js';
-import userRoutes from './routes/user.routes.js';
-import { connectDB } from './utils/feature.js';
-import adminRoutes from './routes/admin.routes.js';
+import { errorMiddleware } from "./middlewares/error.js";
+import chatRoutes from "./routes/chat.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { connectDB } from "./utils/feature.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 dotenv.config({
-    path:'./.env'
+  path: "./.env",
 });
-export const envMode =process.env.NODE_ENV.trim() || "PRODUCTION";
-const mongoURI=process.env.MONGO_URI;
+export const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 connectDB(mongoURI);
 export const adminSecretKey = process.env.ADMIN_SECRET_KEY || "Chat-App-Admin";
@@ -24,25 +24,30 @@ export const adminSecretKey = process.env.ADMIN_SECRET_KEY || "Chat-App-Admin";
 // createGroupChats(10);
 // createMessagesInAChat("668fa8c74c8bd7ab68a7f0ec",50)
 const app = express();
+const server = createServer(app)
+const io = new Server(server,{})
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/admin", adminRoutes);
 
-
-app.use("/user",userRoutes);
-app.use("/chat",chatRoutes);
-app.use("/admin",adminRoutes);
-
-
-app.get('/', (req, res) => {  
-    res.send(`Server is running on http://localhost:${port} in ${envMode} mode`);
+app.get("/", (req, res) => {
+  res.send(`Server is running on http://localhost:${port} in ${envMode} mode`);
 });
 
 
-
-
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+    socket.on("disconnect", () => {
+      console.log("user disconnected", socket.id);
+    });
+});
 
 app.use(errorMiddleware);
-app.listen(port, () => {   
-    console.log(`Server is running on http://localhost:${port}`);
+
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
